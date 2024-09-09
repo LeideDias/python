0f0bfdc07f7f21901370c72be2c50b8b2ccc3376
LeideDias <leidediasarte27@gmail.com>
2024-09-09 19:57:41 -0300

    modificação

--- primeiro.py
+++ primeiro.py
@@ -1,5 +1,5 @@
 print ('Hello, World!')
-print ("Seja bem-vinda, maravilhosa Engenheira de Cloud")
+print ("Seja bem-vinda!")
 
 linguagem = input ('Qual é a linguagem de programação que você está estudando?')
 print ('A linguagem é: ',linguagem)
